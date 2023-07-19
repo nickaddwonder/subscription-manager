7e41a6b10e3c45124be42b774291ec58c09e9ce2
Nick Zou <nick@elevate.ca>
2023-07-19 19:21:43 -0400

    add configure clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs"
+import { UserButton, auth, currentUser } from "@clerk/nextjs"
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+  const user = await currentUser();
   return (
     <main>
       <div><UserButton afterSignOutUrl="/" /></div>
+      <div className="text-white">User Id: {userId}</div>
+      {!user ? <div>User not logged in:</div> : <div>Hello {user?.firstName}</div>}
     </main>
   )
 }
