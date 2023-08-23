717e58f9d302b6a7f8940c0814f7f5799c421545
Nick Zou <nick@elevate.ca>
2023-08-22 21:09:21 -0400

    add title parser function

---
+++ functions/title.ts
@@ -0,0 +1,14 @@
+import Movie from '@/app/types/Movie';
+import TvShow from '@/app/types/TvShow';
+
+const title = (content: Movie | TvShow): string => {
+  if ('name' in content) {
+    return content.name;
+  } else if ('title' in content) {
+    return content.title;
+  } else {
+    throw new Error('Content Type does not contain a name or a title');
+  }
+};
+
+export default title;
