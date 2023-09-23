effc0dcf8b7fcd76c6ce1fe4e0718f3851fcd91d
Nick Zou <nick@elevate.ca>
2023-09-23 15:09:50 -0400

    refactor the test

--- app/_functions/addContentToDatabase/__tests__/addContentToDatabase.test.ts
+++ app/_functions/addContentToDatabase/__tests__/addContentToDatabase.test.ts
@@ -1,12 +1,15 @@
+// addContentToDatabase.test.ts
+
 import addContentToDatabase from '../addContentToDatabase';
 import { database } from '@/app/firebase';
 import { addDoc, collection } from 'firebase/firestore';
+import TvShow from '@/app/_types/TvShow';
 
+// Mock the database and firestore functions
 jest.mock('@/app/firebase', () => ({
   database: 'mocked-database',
 }));
 
-// Mock the firestore functions
 jest.mock('firebase/firestore', () => ({
   addDoc: jest.fn(),
   collection: jest.fn((db, name) => {
@@ -22,35 +25,57 @@ describe('addContentToDatabase', () => {
     jest.clearAllMocks();
   });
 
-  it('should add content and return success and docRef', async () => {
+  it('should add a TvShow content and return success and docRef', async () => {
     const mockDocRef = { id: '1234' }; // Your mock doc reference
+    const mockTvShow: TvShow = {
+      id: 101,
+      name: 'Test Show',
+      original_name: 'Test Original Name',
+      poster_path: '/path/to/poster',
+      popularity: 10.5,
+      backdrop_path: '/path/to/backdrop',
+      vote_average: 8.5,
+      overview: 'This is a test show overview.',
+      origin_country: ['US'],
+      genre_ids: [1, 2, 3],
+      original_language: 'en',
+      vote_count: 200,
+      first_air_date: new Date('2023-01-01'),
+    };
 
     (addDoc as jest.Mock).mockResolvedValueOnce(mockDocRef);
 
-    const result = await addContentToDatabase(
-      { title: 'Test Title' },
-      'some-collection'
-    );
+    const result = await addContentToDatabase(mockTvShow, 'some-collection');
 
     expect(result).toEqual({
       success: true,
       docRef: mockDocRef,
     });
     expect(collection).toHaveBeenCalledWith(database, 'some-collection');
-    expect(addDoc).toHaveBeenCalledWith('mocked-collection', {
-      title: 'Test Title',
-    });
+    expect(addDoc).toHaveBeenCalledWith('mocked-collection', mockTvShow);
   });
 
   it('should catch errors and return success as false', async () => {
     const mockError = new Error('An error occurred');
+    const mockTvShow: TvShow = {
+      id: 101,
+      name: 'Test Show',
+      original_name: 'Test Original Name',
+      poster_path: '/path/to/poster',
+      popularity: 10.5,
+      backdrop_path: '/path/to/backdrop',
+      vote_average: 8.5,
+      overview: 'This is a test show overview.',
+      origin_country: ['US'],
+      genre_ids: [1, 2, 3],
+      original_language: 'en',
+      vote_count: 200,
+      first_air_date: new Date('2023-01-01'),
+    };
 
     (addDoc as jest.Mock).mockRejectedValueOnce(mockError);
 
-    const result = await addContentToDatabase(
-      { title: 'Test Title' },
-      'some-collection'
-    );
+    const result = await addContentToDatabase(mockTvShow, 'some-collection');
 
     expect(result).toEqual({
       success: false,
