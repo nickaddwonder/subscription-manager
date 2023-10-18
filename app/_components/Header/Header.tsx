f91b990f8b7b2c5ebfa3cce1d737a9196c48f7e5
Nick Zou <z.nick00@gmail.com>
2023-10-17 20:21:32 -0400

    refactor Header into arrow function

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from '@clerk/nextjs';
 
-export default function Header() {
+const Header = () => {
   return (
     <div className="w-full flex justify-between items-center p-2 bg-white text-black">
       <span>logo</span>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+
+export default Header;
