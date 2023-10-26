326c16f208e0d1d4c81ad23257b25cfbfbef4afb
Nick Zou <z.nick00@gmail.com>
2023-10-26 18:38:35 -0400

    make text uppercase

--- app/_components/Pill/Pill.tsx
+++ app/_components/Pill/Pill.tsx
@@ -1,12 +1,15 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode } from 'react';
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 const Pill: FC<Props> = ({ children }) => {
-  return <span className="pill text-[#667085] tracking-tighter text-uppercase rounded-2xl bg-[#d0d5dd] py-1 px-3 inline-block leading-none font-bold">{children}</span>
-
+  return (
+    <span className="pill text-[#667085] tracking-tighter uppercase rounded-2xl bg-[#d0d5dd] py-1 px-3 inline-block leading-none font-bold">
+      {children}
+    </span>
+  );
 };
 
 export default Pill;
