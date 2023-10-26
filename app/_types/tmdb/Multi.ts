93346bd6d224cf3394c1c53c4ab77365f0617b1b
Nick Zou <z.nick00@gmail.com>
2023-10-25 20:23:27 -0400

    refactor Multi type

--- app/_types/tmdb/Multi.ts
+++ app/_types/tmdb/Multi.ts
@@ -1,10 +1,12 @@
 import Movie from './Movie';
 import TvShow from './TvShow';
 
-export type MediaType = {
-  media_type: 'tv' | 'movie' | 'person';
+export type MediaType = 'tv' | 'movie' | 'person';
+
+export type MediaTypes = {
+  media_type: MediaType;
 };
 
-type Multi = (Movie | TvShow) & MediaType;
+type Multi = (Movie | TvShow) & MediaTypes;
 
 export default Multi;
